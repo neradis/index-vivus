c459eaca45c925b0720317f19d32b3700f99278b
Andy Wermke <andy@dev.next-step-software.com>
2013-12-09 23:42:29 +0100

    Imported basic rails app.

---
+++ frontend/railsapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IndexVivus::Application.config.secret_token = '4c26ed88b1bd6998c497d33873edfb3a3cf3f509bf7cd530a867be843b7cc82011cd65901b070b41523cd1412855772dd96d7dfbf92f94bcdcad2359a043dc09'
