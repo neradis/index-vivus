804629a9db6495daa488087f08f9f3db505baae3
chillSen <GJxrv(fd6PjdDr0dd}lK>
2013-12-11 16:13:48 +0100

    *added layout, view, css and javascript files

--- frontend/railsapp/app/assets/javascripts/main.js
+++ frontend/railsapp/app/assets/javascripts/main.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+window.onload = function() {
+ hide();
+ $("#selectable").selectable();
+ $("li:first").addClass('ui-selected');
+};
