3581b6d20b475b8799b918602b131dac875e98c0
Andy Wermke <andy@dev.next-step-software.com>
2013-12-12 23:47:25 +0100

    Removed calls to non-existing hide() and show() functions.

--- frontend/railsapp/app/assets/javascripts/main.js
+++ frontend/railsapp/app/assets/javascripts/main.js
@@ -1,7 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 window.onload = function() {
- hide();
  $("#selectable").selectable();
  $("li:first").addClass('ui-selected');
 };
