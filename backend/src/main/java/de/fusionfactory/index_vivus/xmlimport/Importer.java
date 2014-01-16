6a94b30a578a4dfed9d67f255f48099778ea17bf
Kevin Jakob <kevin-jakob@web.de>
2014-01-16 15:49:48 +0100

    * class Importer uses new persistence system * fix for exception detection in SlickTools

--- backend/src/main/java/de/fusionfactory/index_vivus/xmlimport/Importer.java
+++ backend/src/main/java/de/fusionfactory/index_vivus/xmlimport/Importer.java
@@ -1,9 +1,11 @@
 package de.fusionfactory.index_vivus.xmlimport;
 
-import de.fusionfactory.index_vivus.models.IDictionaryEntry;
-import de.fusionfactory.index_vivus.models.ModelFactory;
+import com.google.common.base.Joiner;
+import com.google.common.base.Optional;
+import de.fusionfactory.index_vivus.configuration.Environment;
 import de.fusionfactory.index_vivus.models.WordType;
 import de.fusionfactory.index_vivus.models.scalaimpl.DictionaryEntry;
+import de.fusionfactory.index_vivus.persistence.DbHelper;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
@@ -13,16 +15,23 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import scala.slick.session.Session;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.*;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.List;
+
+import static java.lang.String.format;
 
 /**
  * Author: Kevin Jakob <kevin-jakob@web.de>
@@ -31,13 +40,20 @@ import java.io.StringWriter;
  */
 public class Importer {
     private static Logger logger = Logger.getLogger(Importer.class);
-
+    private final Optional<Integer> integerAbsent = Optional.absent();
+    private final Optional<String> stringAbsent = Optional.absent();
     private long currentEntryCounter = 0;
 
-    public Importer() {}
+    private boolean debug = false;
+
+    public Importer() {
+        if(Environment.getActive().equals(Environment.DEVELOPMENT) || Environment.getActive().equals(Environment.TEST)) {
+            debug = true;
+            DbHelper.createMissingTables();
+        }
+    }
 
     private void parseInnerInformation(Element article) {
-
     }
 
     private void parseMetaData(File inpFile) {
@@ -46,16 +62,20 @@ public class Importer {
 
     private void parseEntryData(NodeList entries) throws IOException, SAXException {
         //foreach entry
+        Optional<DictionaryEntry> prevEntry = Optional.absent();
         for(int i = 0; i < entries.getLength(); i++) {
             long id = ++this.currentEntryCounter;
+            if(this.currentEntryCounter > 3)
+                break;
             //logger.info("Eintrag #" + id + "");
             String keyword = "";
             String description = "";
-            String descriptionHtml = "";
-            WordType wordType = WordType.UNKOWN;
-            int keyGroupIndex = 1;
+            String descriptionHtml = null;
+            WordType wordType = WordType.UNKNOWN;
+            byte keyGroupIndex = 1;
             NodeList entryContent = entries.item(i).getChildNodes();
             for(int c = 0; c < entryContent.getLength(); c ++) {
+
                 Node childContent = entryContent.item(c);
 
                 // keyword
@@ -69,7 +89,7 @@ public class Importer {
                         //keyword without the ordinal number
                         keyword = buf.substring(0,buf.indexOf("[")).trim();
                         //take the number within square brackets
-                        keyGroupIndex = Integer.parseInt(buf.substring(buf.indexOf("[") + 1, buf.indexOf("]")));
+                        keyGroupIndex = Byte.parseByte(buf.substring(buf.indexOf("[") + 1, buf.indexOf("]")));
                     } else if(buf.contains("[*]"))  //@TODO find out what this means for an entry and if information is needed
                         keyword = buf.substring(0,buf.indexOf("[")).trim();
                     else
@@ -94,23 +114,32 @@ public class Importer {
                             } catch (TransformerException e) {
                                 logger.error(e.getStackTrace());
                             }
-                            descriptionHtml += outStr.toString();
+                            //WTF java: adding to the word NULL ???
+                            if(descriptionHtml == null)
+                                descriptionHtml = outStr.toString();
+                            else
+                                descriptionHtml += outStr.toString();
                         }
                     }
                     //strips all the html tags and unescape resulting string from cleaner
                     description = StringEscapeUtils.unescapeHtml4(Jsoup.clean(descriptionHtml, Whitelist.none()));
                 }
             }
-            IDictionaryEntry currentEntry = ModelFactory.createDictionaryEntry(keyword, description, keyGroupIndex);
-            currentEntry.setHtmlDescription(descriptionHtml);
+            DictionaryEntry currentEntry = DictionaryEntry.create(integerAbsent, integerAbsent, keyGroupIndex, keyword, description, Optional.of(descriptionHtml), wordType);
+            ImportTransaction trans = new ImportTransaction(prevEntry,currentEntry);
+            logger.info(trans.getPrevE());
+            DbHelper.transaction(trans);
+            logger.info(trans.getCurrentE());
+            prevEntry = trans.getPrevE();
+
+
+            //IDictionaryEntry currentEntry = ModelFactory.createDictionaryEntry(keyword, description, keyGroupIndex);
+            //    currentEntry.setHtmlDescription(descriptionHtml);
             /* TODO: when implemented, activate these setters:
-              currentEntry.setId(id)
               currentEntry.setWordType(wordType);*/
-            logger.info(((DictionaryEntry)currentEntry).toString());
-            //if(this.currentEntryCounter > 3)
-            //break;
+             //   logger.info(((DictionaryEntry)currentEntry).toString());
+            }
         }
-    }
 
     public void importDir(String fileName) throws IOException, SAXException {
         File dirHandle =new File(fileName);
@@ -140,6 +169,46 @@ public class Importer {
 
                 }
             }
+        }
+    }
+
+    public class ImportTransaction extends DbHelper.Operations<Object> {
+        private Optional<DictionaryEntry> prevE;
+        private DictionaryEntry currentE;
+
+        public ImportTransaction(Optional<DictionaryEntry> prevEntry, DictionaryEntry currentEntry) {
+            prevE = prevEntry;
+            currentE = currentEntry;
+        }
+
+        @Override
+        public Object perform(Session tx) {
+                List<DictionaryEntry> duplicates = currentE.crud(tx).duplicateList();
+                if (duplicates.isEmpty()) { // no duplicate entries!
+                    if (prevE.isPresent()) {
+                        currentE.setPrevId(Optional.of(prevE.get().getId()));
+                    }
+                    currentE = currentE.crud(tx).insertAsNew();
+                    if (prevE.isPresent()) {
+                        prevE.get().setNextId(currentE.getIdOptional());
+                        prevE.get().crud(tx).update();
+                    }
+                    //set current Entry as previous Entry
+                    prevE = Optional.of(currentE);
+                } else {
+                    String dupList = Joiner.on(" \n").join(duplicates);
+                    logger.warn(format("Already found entries in database with same content as %s:%n%s%n - SKIPPED",
+                            currentE, dupList));
+                }
+            return null; //nothing to return
+        }
+
+        public Optional<DictionaryEntry> getPrevE() {
+            return prevE;
+        }
+
+        public DictionaryEntry getCurrentE() {
+            return currentE;
         }
     }
 }
