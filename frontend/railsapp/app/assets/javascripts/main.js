28ead7ede1f55463b72e570d6e73dfabeac9e959
Simon <chillSen@github.com>
2014-01-16 13:39:15 +0100

    added searchKeyword functionality

--- frontend/railsapp/app/assets/javascripts/main.js
+++ frontend/railsapp/app/assets/javascripts/main.js
@@ -4,3 +4,15 @@ window.onload = function() {
  $("#selectable").selectable();
  $("li:first").addClass('ui-selected');
 };
+
+function searchKeyword(){
+ var value = $("#inputKeywordSearch").val();
+ $.getJSON("ajax/keyword/matches/".concat(value),function(result){
+    $("tbody tr").remove();
+    $.each(result, function(i){
+	$('#tbResult > tbody:last').append('<tr><td>'.concat(result[i]["keyword"])+'</td><td>'.concat(result[i]["type"])+'</td><td>'.concat(result[i]["description"])+'</td></tr>');
+    });
+  });
+}
+
+
