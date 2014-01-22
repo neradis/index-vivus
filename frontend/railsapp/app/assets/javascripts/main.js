4a0b8b26a5c6d45398dcad90eb594eb004b36287
Simon <chillSen@github.com>
2014-01-22 11:31:21 +0100

    -changed selector names

--- frontend/railsapp/app/assets/javascripts/main.js
+++ frontend/railsapp/app/assets/javascripts/main.js
@@ -1,17 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 window.onload = function() {
-    $("#selectable").selectable();
+    $("#language-selector").selectable();
     $("li:first").addClass('ui-selected');
 };
 
 function searchKeyword(){
     var value = $("#inputKeywordSearch").val();
-    var lang = $("#selectable .ui-selected").attr("data-lang");
+    var lang = $("#language-selector .ui-selected").attr("data-lang");
     $.getJSON("ajax/keyword/matches/"+ lang +"/"+(value),function(result){
         $("tbody tr").remove();
         $.each(result, function(i){
-	    $('#tbResult > tbody:last').append('<tr><td>'.concat(result[i]["keyword"])+'</td><td>'.concat(result[i]["type"])+'</td><td>'.concat(result[i]["description"])+'</td></tr>');
+	    $('#tbResult > tbody:last').append('<tr><td>' + (result[i]["keyword"])+'</td><td>' + (result[i]["type"])+'</td><td>' + (result[i]["description"])+'</td></tr>');
         });
     });
 }
@@ -21,7 +21,7 @@ function searchFulltext(){
     $.getJSON("ajax/fulltext/matches/"+(value),function(result){
         $("tbody tr").remove();
         $.each(result, function(i){
-	    $('#tbResult > tbody:last').append('<tr><td>'.concat(result[i]["keyword"])+'</td><td>'.concat(result[i]["type"])+'</td><td>'.concat(result[i]["description"])+'</td></tr>');
+	    $('#tbResult > tbody:last').append('<tr><td>' + (result[i]["keyword"])+'</td><td>' + (result[i]["type"])+'</td><td>' + (result[i]["description"]) + '</td></tr>');
         });
     });
 }
