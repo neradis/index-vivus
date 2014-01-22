4fc420ac342154c21c45bc00c894a8906f06bca4
Simon <chillSen@github.com>
2014-01-22 10:37:01 +0100

    -text indent fixes

--- frontend/railsapp/app/assets/javascripts/main.js
+++ frontend/railsapp/app/assets/javascripts/main.js
@@ -1,30 +1,29 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 window.onload = function() {
- $("#selectable").selectable();
- $("li:first").addClass('ui-selected');
+    $("#selectable").selectable();
+    $("li:first").addClass('ui-selected');
 };
 
 function searchKeyword(){
- var value = $("#inputKeywordSearch").val();
- var lang = "LATIN";
- $.getJSON("ajax/keyword/matches/"+ lang +"/"+(value),function(result){
-    $("tbody tr").remove();
-    $.each(result, function(i){
-	$('#tbResult > tbody:last').append('<tr><td>'.concat(result[i]["keyword"])+'</td><td>'.concat(result[i]["type"])+'</td><td>'.concat(result[i]["description"])+'</td></tr>');
+    var value = $("#inputKeywordSearch").val();
+    var lang = $("#selectable .ui-selected").attr("data-lang");
+    $.getJSON("ajax/keyword/matches/"+ lang +"/"+(value),function(result){
+        $("tbody tr").remove();
+        $.each(result, function(i){
+	    $('#tbResult > tbody:last').append('<tr><td>'.concat(result[i]["keyword"])+'</td><td>'.concat(result[i]["type"])+'</td><td>'.concat(result[i]["description"])+'</td></tr>');
+        });
     });
-  });
 }
 
 function searchFulltext(){
- var value = $("#inputFulltextSearch").val();
- var lang = "LATIN";
- $.getJSON("ajax/fulltext/matches/"+(value),function(result){
-    $("tbody tr").remove();
-    $.each(result, function(i){
-	$('#tbResult > tbody:last').append('<tr><td>'.concat(result[i]["keyword"])+'</td><td>'.concat(result[i]["type"])+'</td><td>'.concat(result[i]["description"])+'</td></tr>');
+    var value = $("#inputFulltextSearch").val();
+    $.getJSON("ajax/fulltext/matches/"+(value),function(result){
+        $("tbody tr").remove();
+        $.each(result, function(i){
+	    $('#tbResult > tbody:last').append('<tr><td>'.concat(result[i]["keyword"])+'</td><td>'.concat(result[i]["type"])+'</td><td>'.concat(result[i]["description"])+'</td></tr>');
+        });
     });
-  });
 }
 
 
