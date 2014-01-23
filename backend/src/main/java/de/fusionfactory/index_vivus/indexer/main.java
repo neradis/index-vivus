891ee10f445645b8b357145d3550e753c0b88f55
Eric Kurzhals <ek@attyh.com>
2014-01-23 23:35:45 +0100

    Search functionality implemented

--- backend/src/main/java/de/fusionfactory/index_vivus/indexer/main.java
+++ backend/src/main/java/de/fusionfactory/index_vivus/indexer/main.java
@@ -1,10 +1,14 @@
 package de.fusionfactory.index_vivus.indexer;
 
+import de.fusionfactory.index_vivus.models.scalaimpl.DictionaryEntry;
 import de.fusionfactory.index_vivus.xmlimport.GeorgesImporter;
 import de.fusionfactory.index_vivus.xmlimport.Importer;
+import org.apache.log4j.Logger;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,12 +19,20 @@ import java.io.IOException;
 public class main {
 	public main(String[] args) {
 		Indexer indexer = new Indexer();
-
+		Logger logger = Logger.getLogger(main.class);
 		try {
 //			Importer xmlImporter = new GeorgesImporter();
 //			xmlImporter.importFromDefaultLocation();
 
 			indexer.mapIndexToRam();
+			try {
+				List<DictionaryEntry> entries = indexer.getSearchResults("buchstabe");
+				for (DictionaryEntry e : entries) {
+					logger.info("Found:" + e.getKeyword());
+				}
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
