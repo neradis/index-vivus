fe8ae9e73d13c327a4dd9fec23d2b0421725ff30
Markus Ackermann <neradis@fusionfactory.de>
2014-03-05 12:45:44 +0100

    added parameter validation for the requested page (1 <= page <= max. possible number of pages for request)

--- backend/src/main/java/de/fusionfactory/index_vivus/services/scalaimpl/FullTextSearchService.scala
+++ backend/src/main/java/de/fusionfactory/index_vivus/services/scalaimpl/FullTextSearchService.scala
@@ -18,9 +18,15 @@ class FullTextSearchService extends IFullTextSearchService{
 
     def getMatches(query: String, language: Language = Language.ALL, page: Int, limit: Int): FulltextResultPage = {
 
+      require(page >= 1, "requested page must be => 1, since page counts start form 1")
 
-      val clist = indexer.getSearchResults(query, language, limit, (page -1) * limit )
-      FulltextResultPage(clist.total, limit, page, clist.list)
+      val listWithTotalCount = indexer.getSearchResults(query, language, limit, (page - 1) * limit)
+      val total = listWithTotalCount.total
+
+      def maxPage = total / limit + (if (total % limit > 0) 1 else 0)
+      require(page <= maxPage, s"not enough search results for requested page (max=$maxPage; requested: $page)")
+
+      FulltextResultPage(total, limit, page, listWithTotalCount.list)
     }
 
   def getMatches(query: String, page: Int, limit: Int): FulltextResultPage = getMatches(query, Language.ALL, page, limit)
